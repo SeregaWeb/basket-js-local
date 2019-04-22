8309d56f3eaf9e0d61769a20c71d846f0e80bcb7
student <student@GFL-1428.gfl.geeksforless.net>
2019-04-22 20:17:12 +0300

    add counter

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-
+    console.time('Shop');
     function getLocalSt(){
         var arr = JSON.parse(localStorage.getItem('shop')) || [];
         return arr;
@@ -8,6 +8,15 @@ $( document ).ready(function() {
     var mydata = JSON.parse(data),
     basket = getLocalSt();
 
+    function FullPrices(basket){
+        var FullPrice = 0; 
+        var FullCount = 0;
+        basket.forEach(function(val,key){
+            FullPrice += val.price * val.count;
+            FullCount += +val.count;
+        })
+        $('.priceFull').html(FullPrice+ " $ к оплате \n за "+FullCount+" товаров");
+    }
     function basketShow(basket){
         $('.basket-list').text('');
         basket.forEach(function(val,key){
@@ -17,9 +26,12 @@ $( document ).ready(function() {
             templateBasket.find('.card-name').text(val.name);
             templateBasket.find('.card-bottom__price').text(val.price );
             templateBasket.find('.card-bottom__num').val(val.count);
+            templateBasket.find('.card-bottom__num').attr('id-data' , val.id);
             $('.basket-list').append(templateBasket);
         })
+        FullPrices(basket); 
     }
+
     basketShow(basket);
 
     mydata.forEach(function(val, key ) {
@@ -31,10 +43,39 @@ $( document ).ready(function() {
         
         $('.card-list').append(template);
     });
+    
+    $(document).on('click','.js-count',function(){
+        el = $(this);
+        var id_data = el.attr('id-data');
+        if(el.val() >= 1){
+            basket.forEach(function(val,key){
+                if(val.id == id_data){
+                    val.count = el.val();
+                }
+            })
+            
 
+            
+        }else{
+            alert('нельзя указать отрицательное число'); 
+            return;
+        }
+        localStorage.setItem('shop', JSON.stringify(basket));
+        FullPrices(basket); 
+    })
+    $(document).on('click', '.js-del',function(){
+        id = $(this).closest('li').index();
+        
+        $(this).closest('li').remove();
+        
+        basket.splice(id,1);
+        console.log(basket);
+        localStorage.setItem('shop', JSON.stringify(basket));
+        basketShow(basket);  
+    })
     $(document).on('click','.js-add',function(){
        var card = $(this).closest('li');
-      
+        
        fl = false;
 
        basket.forEach(function(val, key){
@@ -63,5 +104,11 @@ $( document ).ready(function() {
         basketShow(basket);
 
     })
+    console.timeEnd('Shop');
+
   
 });
+
+
+
+
